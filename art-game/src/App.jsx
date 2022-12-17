d37319494c2d54647b50452381fc5b5d4b8ea2df
dminischetti <dminischetti@wsgc.com>
2022-12-16 22:58:06 -0800

    Add basic art support

--- art-game/src/App.jsx
+++ art-game/src/App.jsx
@@ -142,9 +142,31 @@ export function Participants({ state, stateUpdate }) {
 }
 
 export function Art({ state, stateUpdate }) {
+    const art = state.context.art;
+    const isImage = (artwork) => artwork.image?.src;
+    const getImage = (artwork) => artwork.image?.src;
+    const isColor = (artwork) => artwork.color.r && artwork.color.g && artwork.color.b;
+    const getColor = (artwork) => `rgb(${artwork.color.r}, ${artwork.color.g}, ${artwork.color.b})`;
+    console.log(art);
     return (
         <div>
             <h2>Art</h2>
+            <div className="artwork-list">
+                {art.map((artwork, index) => (
+                    <div key={index} className="artwork">
+                        <div>
+                            {isImage(artwork) && <img className="artwork-image" src={getImage(artwork)} alt={artwork.name} />}
+                            {isColor(artwork) && 
+                                <div className="artwork-color" style={{ backgroundColor: getColor(artwork) }}/>
+                            }
+                        </div>
+                        <div className="artwork-name">{artwork.name}</div>
+                        <div className="artwork-artist">{artwork.artist}</div>
+                        <div className="artwork-year">{artwork.year}</div>
+                        <div className="artwork-value">{artwork.value}</div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
--- art-game/src/state/machine.js
+++ art-game/src/state/machine.js
@@ -66,30 +66,34 @@ export default createMachine({
                     cond: (context) => {
                         return context.participants.filter((participant) => participant.role === "collector").length > 0
                     },
-                    actions: [
-                        assign({
-                            participants: (context) => {
-                                const players_min = 4;
-                                const participants = [...context.participants];
-                                const players = context.participants.filter((participant) => participant.role === "collector").length;
-                                const players_missing = players_min - players;
-                                if (players_missing) {
-                                    for (let i = 0; i < players_missing; i++) {
-                                        participants.push({
-                                            id: participants.length,
-                                            name: `Clever AI ${participants.length + 1}`,
-                                            role: 'collector',
-                                        });
-                                    }
-                                }
-                                return participants;
-                            }
-                        })
-                    ],
                 },
             },
         },
         art: {
+            entry: [
+                (context) => {
+                    const art_min = 10;
+                    const art = [...context.art];
+                    const art_missing = art_min - art.length;
+                    if (art_missing) {
+                        for (let i = 0; i < art_missing; i++) {
+                            art.push({
+                                id: art.length,
+                                name: `Art ${art.length + 1}`,
+                                artist: `Artist ${art.length + 1}`,
+                                year: Math.floor(Math.random() * 100) + 1,
+                                price: Math.floor(Math.random() * 1000) + 1,
+                                color: {
+                                    r: Math.floor(Math.random() * 255) + 1,
+                                    g: Math.floor(Math.random() * 255) + 1,
+                                    b: Math.floor(Math.random() * 255) + 1,
+                                },
+                            });
+                        }
+                    }
+                    return context.art = art;
+                }
+            ],
             on: {
                 BACK: 'participants',
                 // on invocation
